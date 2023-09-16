7db2f3a1f9b7b33675512238f7f087b5d7f8cb3c
moses ka <you@example.com>
2023-09-16 08:59:27 +0200

    added router and db

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv"
 import cors from "cors";
-
+import router from "./Router.js";
 dotenv.config();
 
 const app = express();
@@ -10,9 +10,10 @@ const port = process.env.PORT ?? 3000;
 app.use(cors());
 app.use(express.json());
 
-app.get("/api", (_req, res) => {
-    res.send("I demand your request. Now!");
-});
+// app.get("/api", (_req, res) => {
+//     res.send("I demand your request. Now!");
+// });
+app.use("/api", (router));
 
 app.listen(port, () => console.log(`Server is running on port: ${port}`));
 
